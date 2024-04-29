671e04118569df080b2db24e9308313f2178c411
bal16 <ahmadhamidbalya@gmail.com>
2024-04-29 22:37:00 +0700

    add scroll smoot to main div

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -8,14 +8,14 @@ export default function Test() {
                 <nav className="hidden w-2/5 mx-auto text-center border-r md:flex bg-slate-400">
                     Navbar
                 </nav>
-                <div className="w-full mx-auto overflow-y-auto text-center border bg-slate-700">
-                    <header className="h-14 bg-slate-300">Header</header>
+                <div className="w-full mx-auto overflow-y-auto text-center border bg-slate-700 scroll-smooth">
+                    <header className="h-14 bg-slate-300" id="atas">Header</header>
                     <section className="border-y h-36 bg-slate-400">Made a Post?</section>
                     {/* <main className="h-[100rem]"> */}
                     <main className="">
-                        <div className="border-y min-h-[30rem] ">Post</div>
-                        <div className="border-y min-h-[30rem] ">Post</div>
-                        <div className="border-y min-h-[30rem] ">Post</div>
+                        <a href="#atas" className="block border-y min-h-[30rem] ">Post</a>
+                        <a href="#atas" className="block border-y min-h-[30rem] ">Post</a>
+                        <a href="#atas" className="block border-y min-h-[30rem] ">Post</a>
                     </main>
                 </div>
                 <aside className="hidden w-3/5 mx-auto text-center border-l md:flex bg-slate-400">
