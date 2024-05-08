e481b80a4f9c24b78843f2dd2bc6a86a81143051
bal16 <ahmadhamidbalya@gmail.com>
2024-05-08 19:30:38 +0700

    view: make the sidebar sticky

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -47,7 +47,7 @@ export default memo(function Sidebar({}) {
     const number = '12345'
     const message = 'Halo,Admin%0aSaya%20ingin%20tanya%20makanan%20di%20sini%3F'
     return (
-        <aside className="hidden w-3/5 p-2 -mb-1 text-center border-l-[0.1px] border-marshland-950 md:flex text-marshland-950 bg-ecru-white-100 ">
+        <aside className="hidden w-3/5 p-2 -mb-1 text-center border-l-[0.1px] border-marshland-950 md:flex text-marshland-950 bg-ecru-white-100 sticky top-0 h-full">
             <div className="w-full h-full px-3 py-4 text-start">
                 {/* <h2 className="mb-5 text-2xl font-bold text-marshland-950">
                     Sidebar
