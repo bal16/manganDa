4cbd3bf4f20b5c02d0eb4af6703360a0226e1538
bal16 <ahmadhamidbalya@gmail.com>
2024-06-03 22:31:29 +0700

    updTW MINOR

--- resources/js/Components/StoreCard.jsx
+++ resources/js/Components/StoreCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "@inertiajs/react";
 import { memo } from "react";
 function StoreCard({store}) {
     return (
-        <div className="mb-3 shadow-xl w-52 card bg-base-100">
+        <Link href={`/profile/${store.user_id}`} className="mb-3 shadow-xl w-52 card bg-base-100">
             <figure className="px-10 pt-10">
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
@@ -16,7 +17,7 @@ function StoreCard({store}) {
                     <button className="btn btn-primary">See</button>
                 </div> */}
             </div>
-        </div>
+        </Link>
     );
 }
 
