aa5518ec63b0aaece328867643fcc4cbb4988488
bal16 <ahmadhamidbalya@gmail.com>
2024-06-11 16:11:57 +0700

    view: storelist and request

---
+++ resources/js/Pages/StoreBalidate.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import Sidebar from '../partials/Sidebar';
+import Header from '../partials/Header';
+import { Head, Link } from '@inertiajs/react';
+import axios from 'axios';
+
+function StoreValidate({ stores: initialStores }) {
+  const [stores, setStores] = useState(initialStores);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+//   const handleDeleteComment = async (post_id) => {
+//     if (window.confirm('Are you sure you want to delete this post?')) {
+//       try {
+//         await axios.delete(`/post/${post_id}`);
+//         setStores(stores.filter(report => report.post_id !== post_id));
+//         alert('Post has been deleted successfully.');
+//       } catch (error) {
+//         console.error('Error deleting post:', error);
+//         alert('There was an error deleting the post.');
+//       }
+//     }
+//   };
+
+//   const handleAllowComment = async (post_id) =>{
+//     if(window.confirm('Are you sure you want to allow this post?')){
+//       try {
+//         await axios.delete(`/report/${post_id}`);
+//         setStores(stores.filter(report => report.post_id !== post_id));
+//         alert('report has been deleted successfullty');
+//       } catch (error) {
+//         console.error('error: ',error)
+//         alert(`couldn't delete report`)
+//       }
+//     }
+//   }
+
+  const template = (index, content) => (
+    <tr key={content.post_id}>
+      <th>{index + 1}</th>
+      <td>{content.user.name}</td>
+      <td>{content.name}</td>
+      <td>{content.description}</td>
+      <td>{content.address}</td>
+      <td>
+        {/* <Link href={`/profile/${content.user_id}`} className='btn btn-warning'>view</Link> */}
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      {/* Content area */}
+      <div className="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
+        {/*  Site header */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <Head title="Report List" />
+
+        <main>
+          <div className="w-full px-4 py-8 mx-auto sm:px-6 lg:px-8 max-w-9xl">
+            {/* Cards */}
+            <div className="grid grid-cols-1 gap-6">
+              <div className="overflow-x-auto">
+                <table className="table text-center">
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>owner</th>
+                      <th>storename</th>
+                      <th>description</th>
+                      <th>address</th>
+                      <th>action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {stores.map((store, index) => (
+                      template(index, store)
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default StoreList;
